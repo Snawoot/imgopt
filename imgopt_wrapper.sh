e3ec7f7be4b74d121bace57945de6a27a9c3d7df
Vladislav Yarmak <vladislav@vm-0.com>
2017-01-11 23:25:41 +0000

    wrapper: added stop and status functions

--- imgopt_wrapper.sh
+++ imgopt_wrapper.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 approot="$(dirname "$(readlink -f "$0")")"
 pidfile=/var/tmp/imgopt.pid
+op="$1"
 
-(
-    flock -n 9 || {
-        >&2 echo "imgopt is busy now"
-        exit 1
-    }
-    >&2 echo "starting imgopt..."
-    (
-        "$approot"/imgopt "$@" &
-        imgoptpid=$!
-        echo $imgoptpid > "$pidfile"
-        wait $imgoptpid
-        rm "$pidfile"
-    ) </dev/null >/dev/null 2>&1 &
-) 9>/var/tmp/imgopt.lock 
+case $op in
+    stop)
+        [ -f "$pidfile" -a -s "$pidfile" ] && {
+            kill "$(cat "$pidfile")"
+            >&2 echo "killed imgopt."
+        }
+    ;;
+    status)
+        [ -f "$pidfile" -a -s "$pidfile" ] && {
+            cat /proc/"$(cat "$pidfile")"/cmdline | tr '\0' ' '
+            echo
+        } || >&2 echo "imgopt is not running"
+    ;;
+    *)
+        (
+            flock -n 9 || {
+                >&2 echo "imgopt is busy now"
+                exit 1
+            }
+            >&2 echo "starting imgopt..."
+            (
+                "$approot"/imgopt "$@" &
+                imgoptpid=$!
+                echo $imgoptpid > "$pidfile"
+                wait $imgoptpid
+                rm "$pidfile"
+            ) </dev/null >/dev/null 2>&1 &
+        ) 9>/var/tmp/imgopt.lock 
+    ;;
+esac
